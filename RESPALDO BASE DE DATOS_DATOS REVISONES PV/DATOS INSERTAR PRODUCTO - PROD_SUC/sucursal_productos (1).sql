3fce867dba4695c34ef5041c192673ad57942235
Adelaida23 <40848191+Adelaida23@users.noreply.github.com>
2021-06-09 15:23:44 -0500

    09_06_21

---
+++ RESPALDO BASE DE DATOS_DATOS REVISONES PV/DATOS INSERTAR PRODUCTO - PROD_SUC/sucursal_productos (1).sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-06-2021 a las 06:42:16
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `farmacias_gi_prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sucursal_productos`
+--
+
+CREATE TABLE `sucursal_productos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `costo` double(8,2) NOT NULL,
+  `precio` double(8,2) NOT NULL,
+  `existencia` int(10) UNSIGNED NOT NULL,
+  `minimoStock` int(10) UNSIGNED NOT NULL,
+  `status` int(10) UNSIGNED NOT NULL,
+  `idSucursal` bigint(20) UNSIGNED NOT NULL,
+  `idProducto` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sucursal_productos`
+--
+
+INSERT INTO `sucursal_productos` (`id`, `costo`, `precio`, `existencia`, `minimoStock`, `status`, `idSucursal`, `idProducto`, `created_at`, `updated_at`) VALUES
+(1, 35.00, 35.00, 5, 0, 1, 1, 4, '2021-06-07 01:07:47', '2021-06-07 01:49:02'),
+(2, 23.00, 23.00, 8, 0, 1, 1, 6, '2021-06-07 01:08:04', '2021-06-07 01:23:19'),
+(3, 18.00, 18.00, 32, 0, 1, 1, 116, '2021-06-07 01:08:30', '2021-06-07 01:26:45'),
+(4, 20.00, 20.00, 5, 0, 1, 1, 235, '2021-06-07 01:08:42', '2021-06-07 01:08:42'),
+(5, 46.00, 46.00, 0, 0, 1, 1, 257, '2021-06-07 01:09:56', '2021-06-07 01:55:17'),
+(6, 10.00, 10.00, 3, 0, 1, 1, 492, '2021-06-07 01:10:13', '2021-06-07 01:27:26'),
+(7, 72.00, 72.00, 30, 0, 1, 1, 533, '2021-06-07 01:10:26', '2021-06-07 01:28:03'),
+(8, 30.00, 30.00, 20, 0, 1, 1, 1041, '2021-06-07 01:10:41', '2021-06-07 01:29:51'),
+(9, 62.00, 62.00, 30, 0, 1, 1, 989, '2021-06-07 01:10:53', '2021-06-07 01:28:48'),
+(10, 20.00, 20.00, 50, 0, 1, 1, 1022, '2021-06-07 01:11:03', '2021-06-07 01:29:14'),
+(11, 26.00, 26.00, 26, 0, 1, 1, 1038, '2021-06-07 01:11:13', '2021-06-07 01:29:36'),
+(12, 36.00, 36.00, 0, 0, 1, 1, 1245, '2021-06-07 01:11:33', '2021-06-07 01:30:15'),
+(13, 25.00, 25.00, 0, 0, 1, 1, 1444, '2021-06-07 01:11:43', '2021-06-07 01:30:38'),
+(14, 15.00, 15.00, 0, 0, 1, 1, 1809, '2021-06-07 01:11:55', '2021-06-07 01:31:01'),
+(15, 23.00, 23.00, 0, 0, 1, 1, 29, '2021-06-07 01:12:05', '2021-06-07 01:58:26'),
+(16, 13.00, 13.00, 5, 0, 1, 1, 321, '2021-06-07 01:12:18', '2021-06-07 01:33:12'),
+(17, 20.00, 20.00, 25, 0, 1, 1, 659, '2021-06-07 01:12:34', '2021-06-07 01:33:32'),
+(18, 16.00, 16.00, 0, 0, 1, 1, 2053, '2021-06-07 01:12:45', '2021-06-07 01:33:54'),
+(19, 125.00, 125.00, 0, 0, 1, 1, 2783, '2021-06-07 01:12:56', '2021-06-07 01:34:17'),
+(20, 22.00, 22.00, 0, 0, 1, 1, 3685, '2021-06-07 01:13:09', '2021-06-07 01:34:39'),
+(21, 23.00, 23.00, 0, 0, 1, 1, 4305, '2021-06-07 01:13:24', '2021-06-07 01:34:57'),
+(22, 156.00, 156.00, 0, 0, 1, 1, 3108, '2021-06-07 01:13:37', '2021-06-07 01:35:18'),
+(23, 28.00, 28.00, 0, 0, 1, 1, 3616, '2021-06-07 01:13:55', '2021-06-07 01:35:37'),
+(24, 25.00, 25.00, 0, 0, 1, 1, 4137, '2021-06-07 01:14:44', '2021-06-07 01:35:58'),
+(25, 15.00, 15.00, 0, 0, 1, 1, 4327, '2021-06-07 01:15:01', '2021-06-07 01:38:39'),
+(26, 28.00, 28.00, 0, 0, 1, 1, 4258, '2021-06-07 01:15:48', '2021-06-07 01:15:48'),
+(27, 65.00, 65.00, 3, 0, 1, 1, 5, '2021-06-07 01:16:09', '2021-06-07 01:39:09'),
+(28, 15.00, 15.00, 21, 0, 1, 1, 58, '2021-06-07 01:16:19', '2021-06-07 01:39:27'),
+(29, 45.00, 45.00, 24, 0, 1, 1, 154, '2021-06-07 01:16:32', '2021-06-07 01:39:44'),
+(30, 65.00, 65.00, 40, 0, 1, 1, 169, '2021-06-07 01:17:25', '2021-06-07 01:40:11'),
+(31, 35.00, 35.00, 5, 0, 1, 1, 171, '2021-06-07 01:17:37', '2021-06-07 01:40:59'),
+(32, 35.00, 35.00, 4, 0, 1, 1, 184, '2021-06-07 01:17:52', '2021-06-07 01:41:21'),
+(33, 45.00, 45.00, 6, 0, 1, 1, 201, '2021-06-07 01:18:11', '2021-06-07 01:41:41'),
+(34, 110.00, 110.00, 5, 0, 1, 1, 242, '2021-06-07 01:18:23', '2021-06-07 01:42:19'),
+(35, 85.00, 85.00, 12, 0, 1, 1, 276, '2021-06-07 01:18:34', '2021-06-07 01:47:06'),
+(36, 30.00, 30.00, 4, 0, 1, 1, 466, '2021-06-07 01:18:45', '2021-06-07 01:45:54'),
+(37, 35.00, 35.00, 20, 0, 1, 1, 805, '2021-06-07 01:18:56', '2021-06-07 01:44:23'),
+(38, 50.00, 50.00, 0, 0, 1, 1, 1667, '2021-06-07 01:19:13', '2021-06-07 01:44:02'),
+(39, 50.00, 50.00, 0, 0, 1, 1, 2788, '2021-06-07 01:19:24', '2021-06-07 01:43:47');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `sucursal_productos`
+--
+ALTER TABLE `sucursal_productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sucursal_productos_idsucursal_foreign` (`idSucursal`),
+  ADD KEY `sucursal_productos_idproducto_foreign` (`idProducto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `sucursal_productos`
+--
+ALTER TABLE `sucursal_productos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `sucursal_productos`
+--
+ALTER TABLE `sucursal_productos`
+  ADD CONSTRAINT `sucursal_productos_idproducto_foreign` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`id`),
+  ADD CONSTRAINT `sucursal_productos_idsucursal_foreign` FOREIGN KEY (`idSucursal`) REFERENCES `sucursals` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
